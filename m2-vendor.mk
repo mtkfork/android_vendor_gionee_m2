3a8fc2e64bc4ac43d434d70ab32a216ccd96e76e
Diparth Shah <diparths@gmail.com>
2016-05-13 14:23:19 +0530

    initial commit

---
+++ m2-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/m2/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/m2/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/m2/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/m2/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/m2/proprietary/vendor,system/vendor)
